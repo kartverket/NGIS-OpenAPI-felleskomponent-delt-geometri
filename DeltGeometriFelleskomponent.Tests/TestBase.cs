85684f1fade8667b3b5ba3b5416b5fda573f0e5c
Atle Frenvik Sveen <atle.sveen@gmail.com>
2022-09-06 12:35:25 +0200

    fix tests

--- DeltGeometriFelleskomponent.Tests/TestBase.cs
+++ DeltGeometriFelleskomponent.Tests/TestBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using DeltGeometriFelleskomponent.Api.Util;
 using DeltGeometriFelleskomponent.Models;
 
 namespace DeltGeometriFelleskomponent.Tests;
@@ -9,13 +10,7 @@ public abstract class TestBase
     protected static NgisFeature ReadFeature(string path)
     {
         var text = System.IO.File.ReadAllText(path);
-        return JsonSerializer.Deserialize<NgisFeature>(text, new JsonSerializerOptions() {
-            PropertyNameCaseInsensitive = true,
-            Converters = { 
-                new JsonStringEnumConverter(),
-                new NetTopologySuite.IO.Converters.GeoJsonConverterFactory()
-            }
-        })!;
+        return GeoJsonConvert.DeserializeObject<NgisFeature>(text)!;
     }
 
 }
