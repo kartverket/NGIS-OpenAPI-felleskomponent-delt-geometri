2a2339e7094a6883b757a4d40df5a4e196064f0a
Atle Frenvik Sveen <atle.sveen@gmail.com>
2022-09-09 13:28:41 +0200

    compute position for polygon

--- DeltGeometriFelleskomponent.Models/NgisFeature.cs
+++ DeltGeometriFelleskomponent.Models/NgisFeature.cs
@@ -7,7 +7,22 @@ namespace DeltGeometriFelleskomponent.Models;
 public class NgisFeature
 {
     public string Type => "Feature";
-    public Geometry Geometry { get; set; }
+    private Geometry _geometry;
+    public Geometry Geometry
+    {
+        get => _geometry;
+        set
+        {
+            _geometry = value;
+            if (value.GeometryType == "Polygon")
+            {
+                Geometry_Properties ??= new GeometryProperties();
+                Geometry_Properties.Position = new List<double>() { value.Centroid.X, value.Centroid.Y };
+            }
+            
+        }
+    }
+
     public Envelope? BoundingBox { get; set; }
     public AttributesTable Properties { get; set; }
     [JsonProperty("geometry_properties")]
