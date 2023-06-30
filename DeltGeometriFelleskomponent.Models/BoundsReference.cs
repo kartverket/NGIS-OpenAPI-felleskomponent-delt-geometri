0a045eb3c5414ad464518a8a9935cb658ca80d27
Andreas Bergstrøm Aarnseth <andreas.bergstrom.aarnseth@norconsult.com>
2023-06-30 09:57:26 +0200

    Lowercase properties in BoundsReference

--- DeltGeometriFelleskomponent.Models/BoundsReference.cs
+++ DeltGeometriFelleskomponent.Models/BoundsReference.cs
@@ -1,9 +1,18 @@
-﻿namespace DeltGeometriFelleskomponent.Models;
+﻿using Newtonsoft.Json;
+
+namespace DeltGeometriFelleskomponent.Models;
 
 public class BoundsReference
 {
+    [JsonProperty("featuretype")]
     public string Featuretype { get; set; }
+    
+    [JsonProperty("lokalId")]
     public string LokalId { get; set; }
+
+    [JsonProperty("reverse")]
     public bool Reverse { get; set; }
+    
+    [JsonProperty("idx")]
     public List<int> Idx { get; set; }
 }
